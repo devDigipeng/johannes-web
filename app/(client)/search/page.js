35d84a8d28a59781ca05ccce3885ae3bc836199b
gbengaO <gbenga@ojutu.co>
2024-05-08 18:06:38 +0100

    Navigation

--- app/(client)/search/page.js
+++ app/(client)/search/page.js
@@ -9,7 +9,7 @@ const SearchResults = () => {
 
     return (
         <div>
-            <h1>Search Results for "{query}"</h1>
+            {/* <h1>Search Results for "{query}"</h1> */}
         </div>
     );
 };
